473eb1f33b5b15335424bbf76dfa8c4149bf1281
Mendel <mendelengelaer@hotmail.com>
2023-11-21 19:05:36 +0100

    Read fcs file and show header

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import arrr
-from pyscript import document
+import asyncio  
+import pyodide
+import js
+import flowio
+from io import BytesIO
 
+async def get_file(e):
+	files = e.target.files.to_py()
+	for file in files:
+		array_buf = await file.arrayBuffer() # Get arrayBuffer from file
+		file_bytes = array_buf.to_bytes() # convert to raw bytes array 
+		file_content = BytesIO(file_bytes) # Wrap in Python BytesIO file-like object
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+		flow_data = flowio.FlowData(file_content)
+		
+		js.document.getElementById("content").innerHTML = flow_data.header
+
+def main():
+	get_file_proxy = pyodide.ffi.create_proxy(get_file)
+	js.document.getElementById("fileinput").addEventListener("change", get_file_proxy)
+
+main()
